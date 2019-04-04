02b74a57ad27cf42f547d9afb043217c3d7ec90b
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-04 18:13:33 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+    #you'll need to set up another controller action for a user to be able to view this form in the browser.
+  end
+
+  post '/puppy' do
+
+    puppy = Puppy.new(params)
+    @name = puppy.name
+    @breed = puppy.breed
+    @months_old = params["age"]
+    erb :display_puppy
+  end
 end
